f6bc2caae8fff26f4d62a02b77046d065a2a26c7
UB8082 <lucky.ahmad.ft18@mail.umy.ac.id>
2020-12-21 20:06:08 +0700

    Add project files.

---
+++ ClientRest_082_Lucky Ahmad A/ClassData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRest_082_Lucky_Ahmad_A
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1928/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ ClientRest_082_Lucky Ahmad A/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Build.Tasks.Xaml;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRest_082_Lucky_Ahmad_A
+{
+    class Program
+    {
+        string baseUrl = "http://localhost:1907/";
+
+        static void Main(string[] args)
+        {
+            ClassData classData = new ClassData();
+            classData.getData();
+            Program app = new Program();
+            app.buatMahasiswa();
+            Console.ReadLine();
+        }
+
+        void buatMahasiswa()
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            Console.Write("Masukkan Nama: ");
+            mhs.nama = Console.ReadLine();
+            Console.Write("Masukkan NIM: ");
+            mhs.nim = Console.ReadLine();
+            Console.Write("Masukkan Prodi: ");
+            mhs.prodi = Console.ReadLine();
+            Console.Write("Masukkan Angkatan: ");
+            mhs.angkatan = Console.ReadLine();
+
+            var data = JsonConvert.SerializeObject(mhs); //Convert to Json
+            var postData = new WebClient();
+            postData.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            string response = postData.UploadString(baseUrl + "Mahasiswa", data);
+            Console.WriteLine(response);
+        }
+
+
+    }
+
+    [DataContract]
+    public class Mahasiswa
+    {
+        private string _nama, _nim, _prodi, _angkatan;
+        [DataMember]
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+        [DataMember]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+        [DataMember]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+        [DataMember]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+    }
+}
